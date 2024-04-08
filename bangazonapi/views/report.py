844c9a8bde7512895687dc9f43e68dc1f7fe74ae
Rachel Kendricks <rachelanne.kendricks@gmail.com>
2024-04-08 11:43:11 -0500

    customer name displaying on report

--- bangazonapi/views/report.py
+++ bangazonapi/views/report.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from bangazonapi.models import Order, Customer
+from django.contrib.auth.models import User
 
 
 def report(request):
 
     unpaid_orders = Order.objects.filter(payment_type=None)
-    # order_user = Customer.objects.filter(customer_id = id)
+
     report_data = [
-        {"order_id": order.id, "customer_name": order.customer_id}
+        {"order_id": order.id, "customer_name": user.first_name + " " + user.last_name}
         for order in unpaid_orders
+        for customer in Customer.objects.filter(id=order.customer_id)
+        for user in User.objects.filter(id=customer.user_id)
     ]
 
     return render(request, "index.html", {"report_data": report_data})
