736fc2f9e5f01d02a99bf1a0eee473f7aef21504
Rachel Kendricks <rachelanne.kendricks@gmail.com>
2024-04-08 21:53:12 -0500

    price displaying, but not the sum of prices

--- bangazonapi/views/report.py
+++ bangazonapi/views/report.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from bangazonapi.models import Order, Customer
+from bangazonapi.models import Order, Customer, OrderProduct, Product
 from django.contrib.auth.models import User
 
 
@@ -8,11 +8,16 @@ def report(request):
     unpaid_orders = Order.objects.filter(payment_type=None)
 
     report_data = [
-        {"order_id": order.id, "customer_name": user.first_name + " " + user.last_name}
+        {
+            "order_id": order.id,
+            "customer_name": user.first_name + " " + user.last_name,
+            "total_cost": product.price,
+        }
         for order in unpaid_orders
         for customer in Customer.objects.filter(id=order.customer_id)
         for user in User.objects.filter(id=customer.user_id)
-        # for product in
+        for order_product in OrderProduct.objects.filter(id=order.id)
+        for product in Product.objects.filter(id=order_product.product_id)
     ]
 
     return render(request, "index.html", {"report_data": report_data})
