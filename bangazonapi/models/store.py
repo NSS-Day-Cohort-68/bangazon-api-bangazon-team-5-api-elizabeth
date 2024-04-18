e0b7ee41c9ae6bc0ce1473f62bbc6d0b762a7618
Charles Knepshield <aknepshield@gmail.com>
2024-04-18 14:51:00 -0500

    Addressed comments to PR

--- bangazonapi/models/store.py
+++ bangazonapi/models/store.py
@@ -1,11 +1,13 @@
 from django.db import models
-from .customer import User
+from .customer import Customer, User
 
 
 class Store(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name="store")
+    owner = models.OneToOneField(
+        Customer, on_delete=models.CASCADE, related_name="store"
+    )
 
     @property
     def products_sold(self):
